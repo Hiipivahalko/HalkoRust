9594e9b16d99f4c29712eee2e74bb48b719a6f09
Ossi Siipola <siipolaossi@gmail.com>
2023-08-14 16:09:50 +0200

    doc: Added small docs for bitvector struct.

---
+++ doc/source-files.js
@@ -0,0 +1,4 @@
+var sourcesIndex = JSON.parse('{\
+"lib":["",[["bitvectors",[],["mod.rs"]]],["lib.rs"]]\
+}');
+createSourceSidebar();
--- src/bitvectors/mod.rs
+++ src/bitvectors/mod.rs
@@ -4,6 +4,35 @@ use std::ops::{Index, IndexMut};
 #[cfg(test)]
 mod tests;
 
+/// Simple bitvector implementation.
+/// Use struct's build-functions to building/initializing Bitvector
+///
+/// # Example
+///
+/// ```
+/// use halko_rust::bitvectors::Bitvector;
+///
+/// // [0,0,0,0,0]
+/// let mut bv = Bitvector::build_empty(5);
+///
+/// for i in 0..5 { assert_eq!(0, bv.get(i)); }
+///
+/// bv.set(0, 1); // bv = [1,0,0,0,0];
+/// assert_eq!(bv.get(0), 1);
+///
+/// bv.set(3, 1); // bv = [1,0,0,1,0];
+/// assert_eq!(bv.get(3), 1);
+/// ```
+///
+/// * Bitvector can be builded from the array containing 0s and 1s.
+/// ```
+/// use halko_rust::bitvectors::Bitvector;
+///
+/// let a: [u64; 7] = [0,1,0,0,1,1,0];
+/// let bv = Bitvector::build(&a);
+///
+/// for i in 0..a.len() { assert_eq!(a[i], bv.get(i)); }
+/// ```
 #[derive(Debug)]
 pub struct Bitvector {
     data: Vec<u64>,
@@ -11,13 +40,7 @@ pub struct Bitvector {
 }
 
 impl Bitvector {
-    pub fn build(arr: &[u64]) -> Bitvector {
-        Bitvector {
-            data: arr.to_vec(),
-            n: arr.len(),
-        }
-    }
-
+    /// Builds bitvector of length `n` containing only 0s.
     pub fn build_empty(n: usize) -> Bitvector {
         Bitvector {
             data: vec![0; (n/64) + 1],
@@ -26,6 +49,7 @@ impl Bitvector {
 
     }
 
+    /// Builds bitvector from input array containing only 0s and 1s.
     pub fn build(arr: &[u64]) -> Bitvector {
         let mut bv = Bitvector::build_empty(arr.len());
         for (i, v) in arr.iter().enumerate() {
@@ -35,16 +59,21 @@ impl Bitvector {
 
         bv
     }
+
+
+    /// Return length of the bitvector (number of bits).
     pub fn len(&self) -> usize {
         self.n
     }
 
+    /// Returns bit value in the i-th bit.
     pub fn get(&self, i: usize) -> u64 {
         let one: u64 = 1;
         (&self.data[i/64] >> (i%64)) & one
     }
 
-    pub fn set(&mut self, i: usize, val: u32) {
+    /// Sets or unsets the i-th bit in the bitvector.
+    pub fn set(&mut self, i: usize, val: u64) {
         let one: u64 = 1;
         if val == 0 {
             self.data[i/64] &= !(one << (i%64));
