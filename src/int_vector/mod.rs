1efcfe1e75e91bc474811d2d6a6d794654377207
Ossi Siipola <siipolaossi@gmail.com>
2023-09-04 11:28:19 +0200

    feature: Values inside IntVector can be modified with set-function.

--- src/int_vector/mod.rs
+++ src/int_vector/mod.rs
@@ -35,4 +35,44 @@ impl IntVector {
             n,
         }
     }
+
+    pub fn set(&mut self, i: usize, new_val: u64) {
+        if new_val > 0 && new_val.ilog2() as usize > self.l {
+            panic!("[IntVector], Error setting new value into IntVector, new value is too large. \
+                   new value: {}, largest valid value: {}.",
+                   new_val, self.l.pow(2)-1);
+        }
+
+        let k = (i*self.l)/64;
+
+        if self.l == 64 {
+            self.data[k] = new_val;
+            return;
+        }
+
+        let loc_i = (i*self.l)%64;
+
+        // value completely inside the block
+        if loc_i + self.l <= 64 {
+            let left_neg = if self.l + loc_i < 64 {u64::MAX << (self.l + loc_i)} else {0};
+            let right_neg = !(u64::MAX << loc_i);
+            let neg = left_neg | right_neg;
+            // clean prev bits
+            self.data[k] = self.data[k] & neg;
+
+            // set new value
+            self.data[k] = self.data[k] | (new_val << loc_i);
+        } else {
+            // clean data in firts block
+            self.data[k] = self.data[k] & (u64::MAX >> (64-loc_i));
+            // set data in first block
+            self.data[k] = self.data[k] | (new_val << loc_i);
+
+            // clean data in second block
+            self.data[k+1] = self.data[k+1] & (u64::MAX << ((loc_i+self.l)%64));
+            // set data in second block
+            self.data[k+1] = self.data[k+1] | (new_val >> (64-loc_i));
+        }
+
+    }
 }
--- src/int_vector/tests.rs
+++ src/int_vector/tests.rs
@@ -58,3 +58,161 @@ fn new_with_too_large_l_value() {
 fn new_with_too_small_l_value() {
     let _iv = IntVector::new(10, 0);
 }
+
+// fn: set
+#[test]
+fn set_l_64_to_max_and_then_to_zero() {
+    let mut iv = IntVector::new(2,64);
+
+    iv.set(0, u64::MAX);
+    assert_eq!(iv.data[0], u64::MAX);
+    assert_eq!(iv.data[1], 0);
+
+    iv.set(1, u64::MAX);
+    assert_eq!(iv.data[0], u64::MAX);
+    assert_eq!(iv.data[1], u64::MAX);
+
+    iv.set(0, 0);
+    assert_eq!(iv.data[0], 0);
+    assert_eq!(iv.data[1], u64::MAX);
+
+    iv.set(1, 0);
+    assert_eq!(iv.data[0], 0);
+    assert_eq!(iv.data[0], 0);
+}
+
+// fn: set
+#[test]
+fn set_l_4_to_max_all_values_inside_same_block() {
+    let mut iv = IntVector::new(32, 4); // data.len == 2
+
+    for i in 0..16 {
+        iv.set(i, 15);
+        let res = u64::MAX >> (64-((i+1)*4));
+
+        assert_eq!(iv.data[0], res,
+                   "Error when i:{}", i
+                   );
+    }
+    for i in 0..16 {
+        iv.set(i+16, 15);
+        let res = u64::MAX >> (64-((i+1)*4));
+
+        assert_eq!(iv.data[1], res,
+                   "Error when i:{}", i+16
+                   );
+    }
+
+    for i in 0..16-1 {
+        iv.set(i, 0);
+        let res = u64::MAX << ((i+1)*4);
+
+        assert_eq!(iv.data[0], res,
+                   "Error when i:{}", i
+                   );
+    }
+    iv.set(15, 0);
+    assert_eq!(iv.data[0], 0);
+
+    for i in 0..16-1 {
+        iv.set(i+16, 0);
+        let res = u64::MAX << ((i+1)*4);
+
+        assert_eq!(iv.data[1], res,
+                   "Error when i:{}", i+16
+                   );
+    }
+    iv.set(31, 0);
+    assert_eq!(iv.data[1], 0);
+}
+
+// fn: set
+#[test]
+fn set_l_5_to_max_all_values_overlap_blocks() {
+    let mut iv = IntVector::new(39, 5); // data.len == 3
+
+    // set block values to max -> set bits on (1) in every block.
+    for i in 0..12 {
+        iv.set(i, 31);
+        let res = u64::MAX >> (64-((i+1)*5));
+
+        assert_eq!(iv.data[0], res,
+                   "Error when i:{}", i
+                   );
+    }
+    iv.set(12, 31);
+    assert_eq!(iv.data[0], u64::MAX);
+    assert_eq!(iv.data[1], 1);
+
+    for i in 0..12 {
+        iv.set(i+13, 31);
+        let res = u64::MAX >> (64-((i+1)*5)-1);
+
+        assert_eq!(iv.data[1], res,
+                   "Error when i:{}", i+13
+                   );
+    }
+    iv.set(25, 31);
+    assert_eq!(iv.data[1], u64::MAX);
+    assert_eq!(iv.data[2], 3);
+
+    for i in 0..12 {
+        iv.set(i+26, 31);
+        let res = u64::MAX >> (64-((i+1)*5)-2);
+
+        assert_eq!(iv.data[2], res,
+                   "Error when i:{}", i+26
+                   );
+    }
+    iv.set(38, 31);
+    assert_eq!(iv.data[2], u64::MAX);
+    assert_eq!(iv.data[3], 7);
+
+    // set block values to min -> set bits off (0) in every block.
+    for i in 0..12 {
+        iv.set(i, 0);
+        let res = u64::MAX << ((i+1)*5);
+
+        assert_eq!(iv.data[0], res,
+                   "Error when i:{}", i
+                   );
+    }
+    iv.set(12, 0);
+    assert_eq!(iv.data[0], 0);
+    //println!("{}", !1_u64);
+    assert_eq!(iv.data[1], !1_u64);
+
+    for i in 0..12 {
+        iv.set(i+13, 0);
+        let res = u64::MAX << (((i+1)*5)+1);
+
+        assert_eq!(iv.data[1], res,
+                   "Error when i:{}", i+13
+                   );
+    }
+    iv.set(25, 0);
+    assert_eq!(iv.data[1], 0);
+    assert_eq!(iv.data[2], !3_u64);
+
+    for i in 0..12 {
+        iv.set(i+26, 0);
+        let res = u64::MAX << (((i+1)*5)+2);
+
+        assert_eq!(iv.data[2], res,
+                   "Error when i:{}", i+26
+                   );
+    }
+    iv.set(38, 0);
+    assert_eq!(iv.data[2], 0);
+    assert_eq!(iv.data[3], 0);
+}
+
+// fn: set
+#[test]
+#[should_panic]
+fn set_try_to_set_too_large_value() {
+
+    let mut iv = IntVector::new(39, 5); // data.len == 3
+
+    iv.set(0, 64);
+}
